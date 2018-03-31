0e4734d74c0f1dd1746acb5c9f5b0fbee179e117
ppalac <ppalac@outlook.com>
2018-03-31 14:13:59 +0200

    Add project files.

---
+++ IDEOnline-API/Controllers/IDEController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IDEOnline_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class IDEController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("Run")]
+        public IActionResult Run()
+        {
+            return Ok("value1 value2");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Compile([FromBody]string value)
+        {
+        }
+    }
+}
